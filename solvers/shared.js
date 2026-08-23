20fe77a4c02728c0a8a7b143d70163a8ba4b830c
Peter Yu <peter890331@gmail.com>
2026-08-23 23:40:22 +0800

    Delete solvers/shared.js

--- solvers/shared.js
+++
@@ -1,8 +0,0 @@
-window.solverUtils = {
-  sleep: function(ms) { return new Promise(r => setTimeout(r, ms)); },
-  click: function(el) {
-    el.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
-    el.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
-    el.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  }
-};
